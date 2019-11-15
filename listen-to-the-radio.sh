560f3eed4fda1c82011a62aebe574736e0d21945
robinson-philo <robinson-philo@quantifier.org>
2019-11-15 17:29:10 -0500

    added radio, edited readme

---
+++ listen-to-the-radio.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+clear;
+
+read -n 1 -p "What station would you like to listen to? (1=wvfs 2=wnyc 3=wtul q=quit " answer;
+
+clear;
+echo "         ****************************************"
+echo "         **Alright here you go. Happy listening!!"
+echo "         ****************************************"
+
+case $answer in
+    1)
+        mplayer http://voice.wvfs.fsu.edu:8000/stream;;
+    2)
+        mplayer -cache 250  http://fm939.wnyc.org/wnycfm;;
+    3)
+        mplayer -cache 250  http://129.81.156.83:8000/listen;;
+    q|Q)
+        exit;;
+esac
