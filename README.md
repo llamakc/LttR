ca6852cf31226694c6111206cb472f57092f8927
Ken Clark <llamakc@gmail.com>
2019-11-20 17:39:32 -0600

    Modified shebang to respect ENV

--- README.md
+++ README.md
@@ -6,3 +6,5 @@ on a re-install and now they're all saved in one place.
 Also included is a short script that lets you listen to one of a few streaming radio stations from the Command Line. Is it a configuration file? No. Is it an excuse to make a pull request. Yes.
 
 **ECCE HOMO!!!**
+
+Notepad was not used 
--- listen-to-the-radio.sh
+++ listen-to-the-radio.sh
@@ -1,7 +1,8 @@
-#!/bin/bash
+#!/bin/sh
 clear;
 
-read -n 1 -p "What station would you like to listen to? (1=wvfs 2=wnyc 3=wtul q=quit " answer;
+read -n 1 -p "What station would you like to listen to? (1=wvfs 2=wnyc 3=wtul
+q=quit " answer;
 
 clear;
 echo "         ****************************************"
@@ -9,12 +10,13 @@ echo "         **Alright here you go. Happy listening!!"
 echo "         ****************************************"
 
 case $answer in
-    1)
-        mplayer http://voice.wvfs.fsu.edu:8000/stream;;
-    2)
-        mplayer -cache 250  http://fm939.wnyc.org/wnycfm;;
-    3)
-        mplayer -cache 250  http://129.81.156.83:8000/listen;;
-    q|Q)
-        exit;;
+      1)
+                mplayer http://voice.wvfs.fsu.edu:8000/stream;;
+      2)
+                mplayer -cache 250 http://fm939.wnyc.org/wnycfm;;
+      3)
+                mplayer -cache 250 http://129.81.156.83:8000/listen;;
+      q|Q)
+                exit;;
+
 esac
