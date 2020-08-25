3904b246d4fc26c5dfe8289258ba2ec510ffba7a
royaleconqueso <github@quantifier.org>
2020-08-25 14:10:01 -0400

    Update listen-to-the-radio.sh

--- listen-to-the-radio.sh
+++ listen-to-the-radio.sh
@@ -2,6 +2,12 @@
  
 #"listen-to-the-radio.sh" is a member of the //Quantifier//Consortium//. All rights reserved.
 
+
+comment1="##https://github.com/llamakc/Lttr"
+comment2="##Config file for the listen-to-the-radio music player"
+##two separate comments is annoying, but necessary, since not all versions of echo respect the newline parameter, or the -e flag
+
+
 if ! [ -x "$(command -v mplayer)" ]; then
   echo "Oh, no! This script requires that you have mplayer installed and available in your path. You should either install mplayer, or edit the script to include your preferred player. Exiting..." >&2
   exit 1
@@ -31,48 +37,48 @@ case $answer in
       0)
                 mplayer -cache 250 -cache-min 25 -playlist /home/$USER/.lttr;;
       1)
-                echo "##https://github.com/llamakc/Lttr" > /home/$USER/.lttr ;
-                echo "##Config file for the listen-to-the-radio music player" >> /home/$USER/.lttr;
+                echo "$comment1" > /home/$USER/.lttr ;
+                echo "$comment2" >> /home/$USER/.lttr;
                 echo "" >> /home/$USER/.lttr;
                 echo "http://voice.wvfs.fsu.edu:8000/stream" >> /home/$USER/.lttr && mplayer -cache 250 http://voice.wvfs.fsu.edu:8000/stream;;
       2)
-                echo "##https://github.com/llamakc/Lttr" > /home/$USER/.lttr ;
-                echo "##Config file for the listen-to-the-radio music player" >> /home/$USER/.lttr;
+                echo "$comment1" > /home/$USER/.lttr ;
+                echo "$comment2" >> /home/$USER/.lttr;
                 echo "" >> /home/$USER/.lttr;
                 echo "http://129.81.156.83:8000/listen" >> /home/$USER/.lttr && mplayer -cache 250 http://129.81.156.83:8000/listen;;
       3)
-                echo "##https://github.com/llamakc/Lttr" > /home/$USER/.lttr ;
-                echo "##Config file for the listen-to-the-radio music player" >> /home/$USER/.lttr;
+                echo "$comment1" > /home/$USER/.lttr ;
+                echo "$comment2" >> /home/$USER/.lttr;
                 echo "" >> /home/$USER/.lttr;
                 echo "http://fm939.wnyc.org/wnycfm" >> /home/$USER/.lttr && mplayer -cache 250 http://fm939.wnyc.org/wnycfm;;
       4)
-                echo "##https://github.com/llamakc/Lttr" > /home/$USER/.lttr ;
-                echo "##Config file for the listen-to-the-radio music player" >> /home/$USER/.lttr;
+                echo "$comment1" > /home/$USER/.lttr ;
+                echo "$comment2" >> /home/$USER/.lttr;
                 echo "" >> /home/$USER/.lttr;
                 echo "https://tektite.streamguys1.com:5145/wwnolive" >> /home/$USER/.lttr && mplayer -cache 1500 -cache-min 50 https://tektite.streamguys1.com:5145/wwnolive;;
       5)
-                echo "##https://github.com/llamakc/Lttr" > /home/$USER/.lttr ;
-                echo "##Config file for the listen-to-the-radio music player" >> /home/$USER/.lttr;
+                echo "$comment1" > /home/$USER/.lttr ;
+                echo "$comment2" >> /home/$USER/.lttr;
                 echo "" >> /home/$USER/.lttr;
                 echo "http://live-mp3-128.kexp.org:8000/kexp128.mp3" >> /home/$USER/.lttr && mplayer -cache 250 http://live-mp3-128.kexp.org:8000/kexp128.mp3;;
       6)
-                echo "##https://github.com/llamakc/Lttr" > /home/$USER/.lttr ;
-                echo "##Config file for the listen-to-the-radio music player" >> /home/$USER/.lttr;
+                echo "$comment1" > /home/$USER/.lttr ;
+                echo "$comment2" >> /home/$USER/.lttr;
                 echo "" >> /home/$USER/.lttr;
                 echo "http://live-icy.gslb01.dr.dk:80/A/A29H.mp3" >> /home/$USER/.lttr && mplayer -cache 1500 -cache-min 50 http://live-icy.gslb01.dr.dk:80/A/A29H.mp3;;
       7)
-                echo "##https://github.com/llamakc/Lttr" > /home/$USER/.lttr ;
-                echo "##Config file for the listen-to-the-radio music player" >> /home/$USER/.lttr;
+                echo "$comment1" > /home/$USER/.lttr ;
+                echo "$comment2" >> /home/$USER/.lttr;
                 echo "" >> /home/$USER/.lttr;
                 echo "http://stream.wuog.org:8000" >> /home/$USER/.lttr && mplayer -cache 250 -cache-min 25 http://stream.wuog.org:8000;;
       8)
-                echo "##https://github.com/llamakc/Lttr" > /home/$USER/.lttr ;
-                echo "##Config file for the listen-to-the-radio music player" >> /home/$USER/.lttr;
+                echo "$comment1" > /home/$USER/.lttr ;
+                echo "$comment2" >> /home/$USER/.lttr;
                 echo "" >> /home/$USER/.lttr;
                 echo "http://tstv-stream.tsm.utexas.edu:8000/kvrx_livestream" >> /home/$USER/.lttr && mplayer -cache 250 -cache-min 25 http://tstv-stream.tsm.utexas.edu:8000/kvrx_livestream;;
       9)
-                echo "##https://github.com/llamakc/Lttr" > /home/$USER/.lttr ;
-                echo "##Config file for the listen-to-the-radio music player" >> /home/$USER/.lttr;
+                echo "$comment1" > /home/$USER/.lttr ;
+                echo "$comment2" >> /home/$USER/.lttr;
                 echo "" >> /home/$USER/.lttr;
                 echo "http://onair.wfuv.org/onair-hi" >> /home/$USER/.lttr && mplayer -cache 250 -cache-min 25 http://onair.wfuv.org/onair-hi;;
       s|S)
