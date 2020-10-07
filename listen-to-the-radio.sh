1ba09c33b513c1b7df3b056f360850657833e30f
llamakc <llamakc@gmail.com>
2020-10-07 07:58:24 -0500

    cosmetic changes

--- listen-to-the-radio.sh
+++ listen-to-the-radio.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/bash
  
 ######"listen-to-the-radio.sh" is a member of the //Quantifier//Consortium//. All rights reserved.
 
@@ -18,11 +18,11 @@ chmod -x /home/$USER/.lttr;
 clear;
  
 read -n 1 -p "Which college rock or NPR news station would you like to listen to? 
-1=WVFS (Tallahassee)                            2=WTUL (New Orleans)
-3=WNYC (NPR New York)                           4=WWNO (NPR New Orleans)
-5=KEXP (Seatle)                                 6=DR P6 Beat (Denmark)
-7=WUOG Radio (Athens GA)                        8=KVRX (Austin)
-9=WFUV (Bronx, NY)                              0=Last Station Played
+1=WVFS (Tallahassee)		2=WTUL (New Orleans)
+3=WNYC (NPR New York)		4=WWNO (NPR New Orleans)
+5=KEXP (Seatle)			6=DR P6 Beat (Denmark)
+7=WUOG Radio (Athens GA)	8=KVRX (Austin)
+9=WFUV (Bronx, NY)		0=Last Station Played
 q=quit
 ==> ? " answer;
 
